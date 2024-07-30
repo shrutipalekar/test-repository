ac6824749fb751f7ea6fbfe9c884b6719363e09b
Shruti Palekar <shrutipalekar46@gmail.com>
2024-07-30 21:01:39 +0530

    Pom.xml update

---
+++ TempMaven/src/main/java/com/docker/demo/HelloDocker.java
@@ -0,0 +1,10 @@
+package com.docker.demo;
+
+public class HelloDocker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Docker demo in progress....");
+	}
+
+}
